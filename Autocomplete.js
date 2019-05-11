1d372d770cd2db90931408ed086d7fe8729df739
Adam Walter <Waltxr@users.noreply.github.com>
2019-05-11 10:07:18 -0400

    created function to handle arrow key navigation and select-on-enter using a switch statement. calling function in init() to have component listen for specific keystrokes.

--- Autocomplete.js
+++ Autocomplete.js
@@ -41,6 +41,7 @@ export default class Autocomplete {
       const el = document.createElement('li');
       el.classList.add('result');
       el.textContent = result.text;
+      el.setAttribute('tabindex','1')
 
       // Pass the value to the onSelect callback
       el.addEventListener('click', () => {
@@ -65,26 +66,27 @@ export default class Autocomplete {
     return inputEl;
   }
 
-  onHttpQueryChange(query) {
-    return fetch(`${this.options.httpResource}?q=${query}&per_page=${this.options.numOfResults}`, {
-        method: "GET",
-    })
-    .then(response => {
-      return response.json()
-    })
-    .then(res => {
-      console.log(res);
-      let users = res.items
-      .map(user => ({
-        text: user.login,
-        value: user.id
-      }))
-      return users
-    })
-    .then( users => {
-      console.log(users);
-      this.updateDropdown(users)
-    })
+  onHttpQueryChange(query) { // get data with asynchronous fetch if there's a query
+    if (!query=='') {
+      return fetch(`${this.options.httpResource}?q=${query}&per_page=${this.options.numOfResults}`)
+      .then(response => {
+        return response.json()
+      })
+      .then(res => {
+        let users = res.items
+        .map(user => ({
+          text: user.login,
+          value: user.id
+        }))
+        return users
+      })
+      .then( users => {
+        this.updateDropdown(users)
+      })
+    } else if (query=='') {
+      const emptyList = []
+      this.updateDropdown(emptyList)
+    }
   }
 
   createHttpQueryInputEl() {
@@ -99,10 +101,38 @@ export default class Autocomplete {
     return inputEl;
   }
 
+  navigateList() {
+    document.onkeydown = (e) => {
+      let activeEl = document.activeElement
+
+      switch (e.keyCode) {
+        case 40:
+          if (!activeEl.nextSibling) {
+            activeEl.focus() //focus on current element if you are at end of the list.
+          } else if (activeEl.name == 'query') {
+            activeEl.nextSibling.firstChild.focus() //focus on first li element if you are in the input
+          } else {
+            activeEl.nextSibling.focus() //focus on next li element if you are in the middle of the list
+          }
+          break;
+        case 38:
+          if (activeEl.name == 'query') {
+            activeEl.focus() //focus on current element if you are at end of the list.
+          } else if (activeEl.previousSibling == null) {
+             activeEl.parentElement.previousSibling.focus() //focus on input if no more list items
+          } else {
+            activeEl.previousSibling.focus() //focus on previous list item
+          }
+        break;
+        case 13:
+          activeEl.click() //trigger onSelect
+        default:
+      }
+    }
+  }
 
   init() {
-
-    if (this.options.httpResource) {
+    if (this.options.httpResource) { // if httpResource send to different creat input function
       this.inputEl = this.createHttpQueryInputEl();
       this.rootEl.appendChild(this.inputEl)
     } else {
@@ -110,10 +140,12 @@ export default class Autocomplete {
       this.inputEl = this.createQueryInputEl();
       this.rootEl.appendChild(this.inputEl)
     }
-
     // Build results dropdown
     this.listEl = document.createElement('ul');
     this.listEl.classList.add('results');
     this.rootEl.appendChild(this.listEl);
+
+    // Handle navigation with arrow keys
+    this.navigateList()
   }
 }
